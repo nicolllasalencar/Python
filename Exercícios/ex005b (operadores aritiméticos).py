505ef7ea48cb5fdc29b398555bf1778db451f2b5
Nicollas Alencar <nicolllasalencar@gmail.com>
2026-03-10 00:05:47 -0300

    Update ex005b (operadores aritiméticos).py
    Resolução do ex005b

--- Exercícios/ex005b (operadores aritiméticos).py
+++ Exercícios/ex005b (operadores aritiméticos).py
@@ -1,1 +1,6 @@
 #algoritimo que leia um número e mostre seu dobro seu triplo e raiz quadrada
+n = float(input('digite um número:'))
+nx2 = n*2
+nx3 = n*3
+raiz = n**(1/2)
+print('O dobro é:{}\nO triplo é:{}\nA raiz quadrada é {:.3f}'.format(nx2,nx3,raiz))
