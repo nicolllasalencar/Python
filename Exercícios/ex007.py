e58c241b815bb5459ba200c5fd19d1f7fa73f176
Nicollas Alencar <nicolllasalencar@gmail.com>
2026-03-10 15:53:40 -0300

    add ex007

---
+++ Exercícios/ex007.py
@@ -0,0 +1,5 @@
+#Leia dois valores e calcule a média aritimetica de dois valores e aredonde o resultado
+v1 = float(input('Digite o primeiro valor'))
+v2 = float(input('Digite o seguindo valor'))
+v3 = float(v1+v2)/2
+print('A média aritimetica de {} + {} valores é: {:.1f}'.format(v1,v2,v3))
